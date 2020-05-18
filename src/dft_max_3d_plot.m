ef1bdfef26b784fde4ed2a653252ba288c482e9b
MikeLrUC <miguelrabuge12@gmail.com>
2020-05-18 19:33:50 +0100

    Added: Frequency plots X,Y,Z > 0.5

--- src/dft_max_3d_plot.m
+++ src/dft_max_3d_plot.m
@@ -1,4 +1,4 @@
-function dft_max_3d_plot(exp_usr_name)
+function dft_max_3d_plot(exp_usr_name,fs)
     % Getting Workspace Variables
     data_name = exp_usr_name;
     label_name = sprintf("%s_label",exp_usr_name);
@@ -8,14 +8,27 @@ function dft_max_3d_plot(exp_usr_name)
     % Aux vars
     n_activities = length(label);
     times = [[label{:,2}]',[label{:,3}]'];
-    table = zeros(n_activities,3);
+    table = zeros(n_activities,3,2);
     for i = 1:n_activities
         win = window(times(i,2) - times(i,1) + 1);
+        N = length(data(times(i,1):times(i,2)));
+        % Resolution in frequency
+        fo = fs/N;
+        if mod(N,2) == 0
+            n = -N/2: N/2 - 1;
+        else
+            n = -fix(N/2): fix(N/2);
+        end
+        % Linear Frequency Domain
+        freq = n * fo;
         for j = 1:3
-            table(i,j) = max(abs(fftshift(fft(detrend(data(times(i,1):times(i,2),j)).*win))));
+            dft = abs(fftshift(fft(detrend(data(times(i,1):times(i,2),j)).*win)));
+            table(i,j,1) = max(dft);
+            index = dft == table(i,j);
+            freq_max_magnitude = freq(index);
+            table(i,j,2) = freq_max_magnitude(freq_max_magnitude>=0);
         end
     end
-    figure();
     % Getting labels
     labels = string(label);
     labels = labels(:,1);
@@ -27,18 +40,28 @@ function dft_max_3d_plot(exp_usr_name)
     lay_index = find(labels == "LAY");
     stand_index = find(labels == "STAND");
     % Plotting
-    plot3(table(walk_index,1),table(walk_index,2),table(walk_index,3),"bo",'DisplayName',"WALK");
-    hold on
-    plot3(table(walku_index,1),table(walku_index,2),table(walku_index,3),"ro",'DisplayName',"WALK_U_P");
-    plot3(table(walkd_index,1),table(walkd_index,2),table(walkd_index,3),"go",'DisplayName',"WALK_D_O_W_N");
-    plot3(table(stand_index,1),table(stand_index,2),table(stand_index,3),"bx",'DisplayName',"STAND");
-    plot3(table(sit_index,1),table(sit_index,2),table(sit_index,3),"rx",'DisplayName',"SIT");
-    plot3(table(lay_index,1),table(lay_index,2),table(lay_index,3),"gx",'DisplayName',"LAY");
-    % Title
-    title('Tridimensional max DFT plot ['+exp_usr_name+']','Interpreter',"none")
-    % Max DFT values in the signal:
-    xlabel('ACC_X')
-    ylabel('ACC_Y')
-    zlabel('ACC_Z')
-    legend show
+    titles = ["Magnitude","Frequency"];
+    for i = 1:2
+        figure();
+        plot3(table(walk_index,1,i),table(walk_index,2,i),table(walk_index,3,i),"bo",'DisplayName',"WALK");
+        hold on
+        plot3(table(walku_index,1,i),table(walku_index,2,i),table(walku_index,3,i),"ro",'DisplayName',"WALK_U_P");
+        plot3(table(walkd_index,1,i),table(walkd_index,2,i),table(walkd_index,3,i),"go",'DisplayName',"WALK_D_O_W_N");
+        plot3(table(stand_index,1,i),table(stand_index,2,i),table(stand_index,3,i),"bx",'DisplayName',"STAND");
+        plot3(table(sit_index,1,i),table(sit_index,2,i),table(sit_index,3,i),"rx",'DisplayName',"SIT");
+        plot3(table(lay_index,1,i),table(lay_index,2,i),table(lay_index,3,i),"gx",'DisplayName',"LAY");
+        % Title
+        title(titles(i)+' of max(DFT) ['+exp_usr_name+']','Interpreter',"none")
+        % Max DFT values in the signal:
+        xlabel('ACC_X')
+        ylabel('ACC_Y')
+        zlabel('ACC_Z')
+        switch i
+            case 1
+                
+            case 2
+                [x,y] = meshgrid(0:0.1:10);surf(x,0.5+0*y+0*x,y);surf(x,y,0*x+0*y + 0.5);surf(0.5+0*y+0*x,x,y);
+        end
+        legend show;
+    end
 end
